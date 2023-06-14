aecb6564d290c7c9c8c7ba03e9cf42b4c8b8ffb2
Ploszajf <ploszajf@gmail.com>
2023-06-14 23:40:17 +0200

    res type

--- app/fileController.js
+++ app/fileController.js
@@ -7,22 +7,17 @@ const path = require('path');
 
 module.exports = {
     addphoto: (req) => {
-//joining path of directory 
+        
 const directoryPath = path.join(__dirname);
-//passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
+
     files.forEach(function (file) {
-        // Do whatever you want to do with the file
         console.log(file); 
     });
 });
         
         const form = formidable({ multiples: true, uploadDir: "upload", keepExtensions: true });
+        console.log(form)
         form.parse(req, (err, fields, files) => {
 
             console.log(err);
